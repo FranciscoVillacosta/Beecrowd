6d1421de45ae5dbbd09163736d3351aae4ee3459
Francisco Villa <franciscovillaacosta9@gmail.com>
2024-09-23 19:01:09 -0500

    1002.cpp

---
+++ 1002.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include<iomanip>
+using namespace std;
+
+main()
+ {
+ double R,A;
+ cin>>R;
+ A=R*R*3.14159;
+ cout<<"A="<<fixed<<setprecision(4)<<A<<"\n";
+ return 0;
+ 	
+ }
